78abd6a03d86601907bb7ec0a70a49d0b85e8d2a
alvinlaw2 <lawa5@coventry.ac.uk>
2016-11-04 10:57:22 +0000

    I have changed the code to show you guys a basic GUI python code using a module from the standard library called "tkinter" please look into it because this will be very useful

--- Virtual Robot Sim.py
+++ Virtual Robot Sim.py
@@ -1,9 +1,26 @@
-import tkinter
+import tkinter as tk
 
+class Application(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        tk.Frame(master, width=800, height=600)
+        self.pack()
+        self.create_widgets()
 
-top = tkinter.Tk()
+    def create_widgets(self):
+        self.hi_there = tk.Button(self,width=70,height=20)
+        self.hi_there["text"] = "Hello World\n(click me)"
+        self.hi_there["command"] = self.say_hi
+        self.hi_there.pack(side="top")
 
-test = tkinter.Entry 
+        self.quit = tk.Button(self, text="QUIT", fg="red",width=20,height=2,
+                              command=root.destroy)
+        self.quit.pack(side="bottom")
 
+    def say_hi(self):
+        print("hi there, everyone!")
 
-top.mainloop()
+root = tk.Tk()
+app = Application(master=root)
+app.mainloop()
+exit()
